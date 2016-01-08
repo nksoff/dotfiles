6b2358d3502060a205248760ec08357e5d12cd5f
Nikita <nks.off@gmail.com>
2016-01-08 17:55:29 +0300

    update zsh configuration, move prezto to antigen

---
+++ files/zsh/antigen.zsh
@@ -0,0 +1,20 @@
+#
+# [antigen]
+#
+
+source ~/.zsh.antigen/antigen.zsh
+
+_zdotdir_set=${+parameters[ZDOTDIR]}
+if (( _zdotdir_set )); then
+    _old_zdotdir=$ZDOTDIR
+fi
+
+antigen use prezto
+
+if (( _zdotdir_set )); then
+    ZDOTDIR=$_old_zdotdir
+else
+    unset ZDOTDIR
+    unset _old_zdotdir
+fi;
+unset _zdotdir_set
