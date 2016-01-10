d2571f506c4d952e9235248e8d2f4e04245cae9d
Nikita <nks.off@gmail.com>
2016-01-10 23:39:38 +0300

    update zsh configuration, switch to oh-my-zsh plugins

--- files/zsh/antigen.zsh
+++ files/zsh/antigen.zsh
@@ -5,6 +5,16 @@
 source ~/.antigen/antigen.zsh
 
 antigen use oh-my-zsh
+antigen bundle git
+antigen bundle autojump
+antigen bundle composer
+antigen bundle gem
+antigen bundle brew
+antigen bundle nyan
+antigen bundle thefuck
+antigen bundle vagrant
+antigen bundle docker
+
 antigen bundle zsh-users/zsh-completions src
 antigen theme nksoff/slim-zsh-theme slim
 antigen bundle zsh-users/zsh-syntax-highlighting
