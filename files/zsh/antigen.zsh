b8f2bd86af3d30641de2a2915f1706a02efc809f
Nikita <nks.off@gmail.com>
2017-11-27 11:39:47 +0300

    update antigen (.antigenrc)

--- files/zsh/antigen.zsh
+++ files/zsh/antigen.zsh
@@ -2,20 +2,5 @@
 # [antigen]
 #
 
-source ~/.antigen/antigen.zsh
-
-antigen use oh-my-zsh
-antigen bundle git
-antigen bundle composer
-antigen bundle gem
-antigen bundle brew
-antigen bundle vagrant
-antigen bundle docker
-
-if [[ -n $commands[thefuck] ]]; then
-    antigen bundle thefuck
-fi
-
-antigen bundle zsh-users/zsh-completions src
-antigen theme nksoff/muslim muslim
-antigen bundle zsh-users/zsh-syntax-highlighting
+source ~/antigen.zsh
+antigen init ~/.antigenrc
